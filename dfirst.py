07c4dab98755a7fead51a4443317d382122c785c
AdilesWolfe <156231227+AdilesWolfe@users.noreply.github.com>
2024-01-26 01:25:46 +0530

    now the graph has the title of the Stock code

--- dfirst.py
+++ dfirst.py
@@ -8,7 +8,7 @@ import datetime
 start= str((datetime.datetime.today()-datetime.timedelta(days=365)).strftime("%d-%m-%Y"))
 end=str(datetime.datetime.today().strftime("%d-%m-%Y"))
 
-sym="SBIN"
+sym="NHPC"
 ser="EQ"
 df=equity_history(sym,ser,start,end)
 #print(df.head())
@@ -17,7 +17,8 @@ fig=go.Figure(data=[go.Candlestick(x=df['CH_TIMESTAMP'],
                     open=df['CH_OPENING_PRICE'],
                     high=df['CH_TRADE_HIGH_PRICE'],
                     low=df['CH_TRADE_LOW_PRICE'],
-                        close=df['CH_CLOSING_PRICE'])])                   
+                        close=df['CH_CLOSING_PRICE'])],
+              layout=dict(title=sym))                   
 
 
 app= dash.Dash()
@@ -25,6 +26,14 @@ app= dash.Dash()
 app.layout= html.Div([
     html.Div(html.H1(children="Hello World")),
     html.Label("DASH GRAPH"),
+    html.Div(
+        dcc.Input(
+            id='stock_input',
+            placeholder='Enter a valid Indian Stock Code',
+            type='text',
+            value=''
+        )
+    ),
     html.Div(
         dcc.Graph(id="Stock Chart",
                   figure=fig),
