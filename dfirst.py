c48eac9c59c9456bcece33d688559a94b1211ac4
AdilesWolfe <156231227+AdilesWolfe@users.noreply.github.com>
2024-01-26 00:12:03 +0530

    dash with candlestick fig

--- dfirst.py
+++ dfirst.py
@@ -1,12 +1,35 @@
 import dash
-import dash_core_components as dcc
+from dash import dcc
 from dash import html
+from nsepython import equity_history
+import plotly.graph_objects as go
+import datetime
+
+start= str((datetime.datetime.today()-datetime.timedelta(days=365)).strftime("%d-%m-%Y"))
+end=str(datetime.datetime.today().strftime("%d-%m-%Y"))
+
+sym="SBIN"
+ser="EQ"
+df=equity_history(sym,ser,start,end)
+#print(df.head())
+
+fig=go.Figure(data=[go.Candlestick(x=df['CH_TIMESTAMP'],
+                    open=df['CH_OPENING_PRICE'],
+                    high=df['CH_TRADE_HIGH_PRICE'],
+                    low=df['CH_TRADE_LOW_PRICE'],
+                        close=df['CH_CLOSING_PRICE'])])                   
+
 
 app= dash.Dash()
 
-app.layout= html.Div(
-    html.H1(children="Hello World")
-)
+app.layout= html.Div([
+    html.Div(html.H1(children="Hello World")),
+    html.Label("DASH GRAPH"),
+    html.Div(
+        dcc.Graph(id="Stock Chart",
+                  figure=fig),
+    )
+])
 
 if __name__== "__main__":
     app.run_server(debug=True)
