e10664b1a79ab4bdda5abbfa0da2189977b117aa
AdilesWolfe <156231227+AdilesWolfe@users.noreply.github.com>
2024-03-08 22:16:49 +0530

    working model with indian stock and forecast. empty graphs are hidden on load but alerts not working at the moment. WILL FIX SOON!

--- Working Model.py
+++ Working Model.py
@@ -8,6 +8,7 @@ from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 from datetime import datetime as dt
 from AI_model import prediction
+import yfinance  as yf
 
 # Read the image file
 with open("Assets/stock-icon.png", "rb") as f:
@@ -16,7 +17,7 @@ with open("Assets/stock-icon.png", "rb") as f:
 # Encode the image as a base64 string
 encoded_image = base64.b64encode(image_data).decode()
 
-def get_stock_graph(df):
+def get_indian_stock_graph(df):
      Candlefig = go.Candlestick(
             x=df['CH_TIMESTAMP'],
             open=df['CH_OPENING_PRICE'],
@@ -26,15 +27,17 @@ def get_stock_graph(df):
         )
      return Candlefig
 
-app = dash.Dash()
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div(
     [
         html.Div(
             [
-                html.H1(children="Stock App"),
+                html.H1(children="InvestIQ"),
+                html.A(href='#', title="About Us"),
                 html.Img(
-                    src=f"data:image/png;base64,{encoded_image}", style={'width': '100px'})
+                    src=f"data:image/png;base64,{encoded_image}", style={'width': '100px'},
+                    alt="Stock image"),
             ],
             className="banner",
         ),
@@ -49,29 +52,29 @@ app.layout = html.Div(
                     value=''
                 ),
                 html.Button("Submit", id="submit-button", n_clicks=0),
-                
-            ],
-            className="input"
-        ),
-        html.Div([
-            dcc.DatePickerRange(id='date-picker',
+                html.Br(),
+                dcc.DatePickerRange(id='date-picker',
                 display_format=('DD/MM/YYYY'),
                 min_date_allowed=dt(1995, 8, 5),
                 max_date_allowed=dt.now(),
                 initial_visible_month=dt.now(),
-                end_date=dt.now().date())
+                end_date=dt.now().date(),
+                style={'margin-top':'15px'})
             ], 
             className="input"
         ),
         html.Div(
-            dcc.Graph(id="Stock Chart", figure={}),
-            className="frame",
-        ),
-        html.Div(
-            id='alert-container',
+            id='alert-container1',
             children=[
-                dbc.Alert(id='alert', children='⚠️Invalid Stock Code⚠️', color="warning", is_open='False', dismissable="True")
-            ], className='alert-container' ),
+                dbc.Alert(id='alert1', children='⚠️Invalid Stock Code⚠️', color='warning', is_open='False', dismissable="True"),
+                dbc.Alert(id='alert2', children='⚠️Please enter Date⚠️', color='warning',dismissable=True, is_open=False )
+            ], className='alert-container'),
+        html.Div(
+            id='Stock Chart'
+        ),
+        html.Br(),
+        html.Br(),
+        html.Br(),
         html.Div([
             html.Label("Enter number of days to forecast"),
             html.Br(),
@@ -82,27 +85,30 @@ app.layout = html.Div(
             html.Button('Forecast', id='forecast-btn', n_clicks=0)
             ], className="input"
         ),
+        html.Br(),
         html.Div(
-            dcc.Graph(id='forecast-graph', figure={}),
-            className='frame'
-        )
+            id='forecast-graph'
+        ),
     ],
     className="main-div"
 )
 
 # callback for stock chart 
 @app.callback(
-    [Output('Stock Chart', 'figure'), Output('alert', 'is_open')],
+    [Output('Stock Chart', 'children'), Output('alert1', 'is_open'), Output('alert2', 'is_open')],
     [Input('submit-button', 'n_clicks'), Input('date-picker', 'start_date'), Input('date-picker', 'end_date')],
     [State('stock_input', 'value')]
 )
 def update_chart(n_clicks, start_date, end_date, stocks):
     if  not n_clicks:
-        return {}, False
+        return {}, False, False
+    
+    if not start_date:
+        return {}, False, True
     
     if stocks==None:
-        raise PreventUpdate
-        return {}, True
+        raise PreventUpdate  
+        return {}, True, False
     else:
         try:
             start_date = dt.strptime(start_date.split('T')[0], '%Y-%m-%d').strftime('%d-%m-%Y')
@@ -112,29 +118,32 @@ def update_chart(n_clicks, start_date, end_date, stocks):
                 raise Exception(f"No data found for {stocks}")
         except  Exception as e:
             print(e)
-            return {
+            return dcc.Graph({
             'data': [],
             'layout': {'title': 'No Data Entered'}
-            }, True
+            }), True, False
 
     ser = "EQ"
     df = equity_history(stocks, ser, start_date, end_date)
-    fig={'data':[get_stock_graph(df)],
+    fig={'data':[get_indian_stock_graph(df)],
          'layout':dict(title=("📊"+stocks+"📊"), height=500, margin=dict(l=100, r=0, t=50, b=0))}
    
-    return fig, False
+    return dcc.Graph(id='Stock Chart', figure=fig, className='frame'), False, False
 
 # Callback to generate forecast and display
 @app.callback(
-    [Output('forecast-graph', 'figure')],
+    [Output('forecast-graph', 'children')],
     [Input('forecast-btn', 'n_clicks')],
     [State('forecast-input','value'), State('stock_input', 'value')]
 )
 def generate_forecast(n_clicks,days,stocks):
     if not n_clicks or days is None or stocks is None :
-        return {}
+        return {
+            'data':[],
+            'layout':{'title':'Please enter all the details.'},
+        }
     else:
         fig=(prediction(stocks, days))
-    return [fig]
+    return [dcc.Graph(id='forecast-graph' ,figure=fig, className='frame')]
 if __name__ == "__main__":
     app.run_server(debug=True)
